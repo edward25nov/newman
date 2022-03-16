ea0022f08121ba21a666a513d7760911ce3a0dac
Edward Gomez <edward25nov@gmail.com>
2022-03-15 22:02:40 -0600

    add example using minimist

--- data.js
+++ data.js
@@ -1,12 +1,22 @@
-var newman = require('newman'); // require Newman in your project
-
+const args = require('minimist')(process.argv.slice(2));
+var newman = require('newman'); 
 var repPath = 'reports/';
+var optionsArray;
 
-var optionsArray = {
-    collection: require('./collections/Using data files.postman_collection.json'),
-    iterationData: require('./data/data.json'),
-    reporters: ['cli', 'htmlextra', 'junit'],
-    reporter: { htmlextra: { export: repPath.concat('data.html'), title: 'Sesion 2 Report' }, junit: { export: repPath.concat('junit.xml') } }
+if(args.d){ // node data.js -d
+    optionsArray = {
+        collection: require('./collections/Using data files.postman_collection.json'),
+        iterationData: require('./data/data.json'),
+        reporters: ['cli', 'htmlextra', 'junit'],
+        reporter: { htmlextra: { export: repPath.concat('data.html'), title: 'Sesion 2 Report' }, junit: { export: repPath.concat('junit.xml') } }
+    }
+}else{
+    optionsArray = {
+        collection: require('./collections/Restful Booker.postman_collection.json'),
+        environment: require('./environments/Production.postman_environment.json'),
+        reporters: ['cli', 'htmlextra', 'junit'],
+        reporter: { htmlextra: { export: repPath.concat('index.html'), title: 'Sesion 2 Report' }, junit: { export: repPath.concat('junit.xml') } }
+    }
 }
 
 newman.run(optionsArray, function (err) {
