f363e684fb401c3eabdf78f3bb8ff3e199eaff52
Edward Gomez <edward25nov@gmail.com>
2022-03-15 23:06:58 -0600

    code format was applied

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-var newman = require('newman'); // require Newman in your project
-
+const newman = require('newman');
 var repPath = 'reports/';
 
 var optionsArray = {
     collection: require('./collections/Restful Booker.postman_collection.json'),
     environment: require('./environments/Production.postman_environment.json'),
     reporters: ['cli', 'htmlextra', 'junit'],
-    reporter: { htmlextra: { export: repPath.concat('index.html'), title: 'Sesion 2 Report' }, junit: { export: repPath.concat('junit.xml') } }
+    reporter: {
+        htmlextra: {
+            export: repPath.concat('index.html'),
+            title: 'Sesion 2 Report'
+        },
+        junit: {
+            export:
+                repPath.concat('junit.xml')
+        }
+    }
 }
 
 newman.run(optionsArray, function (err) {
--- data.js
+++ data.js
@@ -1,21 +1,37 @@
 const args = require('minimist')(process.argv.slice(2));
-var newman = require('newman'); 
+const newman = require('newman');
 var repPath = 'reports/';
 var optionsArray;
 
-if(args.d){ // node data.js -d
+if (args.d) { // node data.js -d
     optionsArray = {
         collection: require('./collections/Using data files.postman_collection.json'),
         iterationData: require('./data/data.json'),
         reporters: ['cli', 'htmlextra', 'junit'],
-        reporter: { htmlextra: { export: repPath.concat('data.html'), title: 'Sesion 2 Report' }, junit: { export: repPath.concat('junit.xml') } }
+        reporter: {
+            htmlextra: {
+                export: repPath.concat('data.html'),
+                title: 'Sesion 2 Report'
+            },
+            junit: {
+                export: repPath.concat('junit.xml')
+            }
+        }
     }
-}else{
+} else {
     optionsArray = {
         collection: require('./collections/Restful Booker.postman_collection.json'),
         environment: require('./environments/Production.postman_environment.json'),
         reporters: ['cli', 'htmlextra', 'junit'],
-        reporter: { htmlextra: { export: repPath.concat('index.html'), title: 'Sesion 2 Report' }, junit: { export: repPath.concat('junit.xml') } }
+        reporter: {
+            htmlextra: {
+                export: repPath.concat('index.html'),
+                title: 'Sesion 2 Report'
+            },
+            junit: {
+                export: repPath.concat('junit.xml')
+            }
+        }
     }
 }
 
